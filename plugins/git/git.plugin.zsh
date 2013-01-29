b3a3ab49ca2a1dcd784b4d6e84e4688fc91640e1
Cristian <cristian@elhacker.net>
2013-01-28 21:41:43 -0800

    Changed git default log Colored maven by default

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,7 +1,7 @@
 # Aliases
 alias g='git'
 compdef g=git
-alias gst='git status'
+alias gst='git status -sb'
 compdef _git gst=git-status
 alias gd='git diff'
 compdef _git gd=git-diff
@@ -58,7 +58,7 @@ alias glg='git log --stat --max-count=5'
 compdef _git glg=git-log
 alias glgg='git log --graph --max-count=5'
 compdef _git glgg=git-log
-alias glgga='git log --graph --decorate --all'
+alias glgga='git log --graph --decorate --all --abbrev-commit --oneline'
 compdef _git glgga=git-log
 alias glo='git log --oneline'
 compdef _git glo=git-log
